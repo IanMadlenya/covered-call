97ab6cdaa9563b66722d0ff9ec71895f78ccf990
Wes Devauld <wes@devauld.ca>
2009-09-11 12:17:54 -0600

    utility classes and specs

--- lib/covered-call/calcs.rb
+++ lib/covered-call/calcs.rb
@@ -10,6 +10,20 @@ module CoveredCallCalcs
 	# expiry is a date of the form "YYYY-MM"
         # this method returns the ceiling of the number of days from today
         # to the third friday of the month
-        1	
-   end
+       
+	# If the first is a Friday, then the earliest expiration is the 15th
+	first_candidate = Date.parse(expiry + "-15")
+	# If the first is a Saturday, then the lastest expiration is the 21st 
+        last_candidate = Date.parse(expiry + "-21")			
+	expiry = nil
+	
+	(first_candidate..last_candidate).each do |candidate|
+		if candidate.wday == 5 then
+			expiry = candidate
+			break
+		end
+	end
+	if expiry == Date.today then return 1 end
+   	(expiry - Date.today).ceil
+    end
 end
--- spec/covered-call_spec.rb
+++ spec/covered-call_spec.rb
@@ -4,7 +4,7 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 describe CoveredCall do
 end
 
-describe CoveredCallCalcs do
+describe CoveredCallCalcs, " annualized return calculator" do
 	include CoveredCallCalcs
 
 	it "should calculate annualized returns" do
@@ -16,6 +16,30 @@ describe CoveredCallCalcs do
 
 		# Make 1% in 10 days
 		annualized_return(0.01, 10). should be_close(0.4379, 0.0001)
+	end
+end
+
+describe CoveredCallCalcs, " days until expiry calculator" do
+	include CoveredCallCalcs
+
+	it "should determine days if the expiry is still to happen this month" do
+		Date.stub!(:today).and_return(Date.parse("2009-09-11"))
+	        days_til_expiry("2009-09").should equal(7)	
+	end
+
+	it "should determine days if the expiration is next month" do
+		Date.stub!(:today).and_return(Date.parse("2009-08-01"))
+	        days_til_expiry("2009-09").should equal(48)	
+	end
+
+	it "should return 1, if the expiration is tomorrow" do
+		Date.stub!(:today).and_return(Date.parse("2009-09-17"))
+		days_til_expiry("2009-09").should equal(1)		
+	end
+	
+	it "should return 1, if the expiration is today" do
+		Date.stub!(:today).and_return(Date.parse("2009-09-18"))
+		days_til_expiry("2009-09").should equal(1)		
 	end
 end
 
